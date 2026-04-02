d24a3235e37920445b5bfedfb72df4bc1c92160a
lukevb2000 <l.vanboven@umcutrecht.nl>
2026-04-02 13:56:10 +0200

    Add assertions for EleveldPatient test SAP and DAP

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,9 +8,18 @@ def test_base_values():
                               base_hr=4, base_map=6, base_pp=2)
     patient3 = EleveldPatient(35, 70, 170, 'male', opiates=True,
                               base_hr = None, base_map=6, base_pp=2)
+    patient4 = EleveldPatient(35, 70, 170, 'male', opiates=True,
+                              base_sap=130, base_dap=80)
 
     assert patient1.base_tpr is None
     assert patient1.base_sv is None
     assert patient2.base_sv == 3
     assert patient2.base_tpr == 0.5  # 6 / (4 * 3)
     assert patient3.base_tpr is None
+    assert patient4.base_pp == 50
+    assert patient4.base_map == (130 + 2 * 80) / 3.0
+    assert patient4.base_sv == 75
+    
+
+
+
